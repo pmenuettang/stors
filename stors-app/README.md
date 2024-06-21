6bd8184c6a5b6dc89aac824484d611d77e726dc6
Philippe Menuet Tang <philippe@ouicloud.software>
2024-06-21 09:18:58 +0200

    feat: add app project as entry point

---
+++ stors-app/README.md
@@ -0,0 +1,17 @@
+<!-- cargo-sync-readme start -->
+
+# Stors
+
+An online store as a pet project.
+
+It implements
+- Hexagonal Architecture
+- Event sourcing
+- CQRS
+
+Sofware Stack
+- Leptos
+- Axum
+- Postgres
+
+<!-- cargo-sync-readme end -->
---
+++ stors-app/src/main.rs
@@ -0,0 +1,17 @@
+//! # Stors
+//!
+//! An online store as a pet project.
+//!
+//! It implements
+//! - Hexagonal Architecture
+//! - Event sourcing
+//! - CQRS
+//!
+//! Sofware Stack
+//! - Leptos
+//! - Axum
+//! - Postgres
+
+fn main() {
+    println!("Hello, world!");
+}
